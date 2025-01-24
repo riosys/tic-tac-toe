6048f419ee29bdc8d666a869f97c58d85328395c
Armando Rios <rios25866@gmail.com>
2025-01-24 06:13:46 -0800

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  base: '/tic-tac-toe/', // Cambia "nombre-del-repo" por el nombre de tu repositorio
-  build: {
-    outDir: 'dist', // Carpeta donde se genera el build
-  },
-})
