397a6fd20f95c5e9ec25c94b02de3155eb2acacb
Armando Rios <rios25866@gmail.com>
2025-01-24 06:08:20 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base: '/tic-tac-toe/', // Cambia "nombre-del-repo" por el nombre de tu repositorio
+  build: {
+    outDir: 'dist', // Carpeta donde se genera el build
+  },
   plugins: [
     tailwindcss(),
   ],
